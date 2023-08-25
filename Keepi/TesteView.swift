0049ebec9eb851d5d87bd5c749f00611fe9cfe9e
andreaoquendo <oquendo.3455@gmail.com>
2023-08-25 09:55:15 -0300

    Create README.md

---
+++ Keepi/TesteView.swift
@@ -0,0 +1,20 @@
+//
+//  TesteView.swift
+//  Keepi
+//
+//  Created by Andrea Oquendo on 25/08/23.
+//
+
+import SwiftUI
+
+struct TesteView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct TesteView_Previews: PreviewProvider {
+    static var previews: some View {
+        TesteView()
+    }
+}
