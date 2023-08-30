358e6b3d4c83f4a63811f1d0d088dfeb0c5178dc
andreaoquendo <oquendo.3455@gmail.com>
2023-08-30 08:50:40 -0300

    feat: tags and feeelings structure

---
+++ Keepi/Classes/Tags.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+
+struct Tag: Identifiable, Hashable {
+    let id = UUID()
+    let name: String
+    
+}
+
+struct Tags {
+
+    static func getTags() -> [Tag] {
+        var allTags: [Tag] = [
+            Tag(name: "Estava com vontade"),
+            Tag(name: "Saí com os amigos"),
+            Tag(name: "Comprei por impulso")
+        ]
+        
+        return allTags
+    }
+}
