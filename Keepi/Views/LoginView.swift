1f67e6a06d9520ad74e88f1a560aaa90118be1ae
Diego Henrick <diegohenrick@Candidos-Macbook-Pro.local>
2023-09-01 10:45:07 -0300

    added NewEnvelopeView

---
+++ Keepi/Views/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  Keepi
+//
+//  Created by Diego Henrick on 31/08/23.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
---
+++ Keepi/Views/NewEnvelopeView.swift
@@ -0,0 +1,20 @@
+//
+//  NewEnvelopeView.swift
+//  Keepi
+//
+//  Created by Diego Henrick on 31/08/23.
+//
+
+import SwiftUI
+
+struct NewEnvelopeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewEnvelopeView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewEnvelopeView()
+    }
+}
