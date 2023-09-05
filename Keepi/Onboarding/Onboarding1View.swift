096a5776c429cf6b7995422da0f221cc56118b9f
Julia Elice <juliajurc@hotmail.com>
2023-09-05 08:57:03 -0300

    Onboarding criado (ainda nao comecado)

---
+++ Keepi/Onboarding/Onboarding1View.swift
@@ -0,0 +1,20 @@
+//
+//  Onboarding1View.swift
+//  Keepi
+//
+//  Created by Julia Elice Jurczyszyn on 05/09/23.
+//
+
+import SwiftUI
+
+struct Onboarding1View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Onboarding1View_Previews: PreviewProvider {
+    static var previews: some View {
+        Onboarding1View()
+    }
+}
